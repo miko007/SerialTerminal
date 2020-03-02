41f3ccc5d43d1b8e8371798f2b7067f6179b6479
Michael Ochmann <ochmannm@hochschule-trier.de>
2020-03-02 10:12:40 +0100

    refined for usage with real serial terminal.
    now has character output, prompt, backspace functionality and more

--- SerialTerminal.hpp
+++ SerialTerminal.hpp
@@ -2,17 +2,19 @@
 
 #define ST_VERSION "1.0.2"
 
+#include <Arduino.h>
+
 namespace maschinendeck {
 
   #if not defined ST_FLAG_NOBUILTIN && defined E2END
   #include "EEPROM.h"
   void printEEPROM(String opts) {
-    Serial.print("offset\t");
+    Serial.print("offset \t");
     for (uint8_t h = 0; h < 16; h++) {
         Serial.print(h, HEX);
         Serial.print('\t');
     }
-    Serial.print('\n');
+    Serial.print("\r\n");
     for (uint8_t i = 0; i < (E2END / 16); i++) {
       String line = "";
       Serial.print(i * 16, HEX);
@@ -24,7 +26,7 @@ namespace maschinendeck {
         line += static_cast<char>(value);
       }
       Serial.print(line);
-      Serial.print('\n');
+      Serial.print("\r\n");
     }
   }
   #endif
@@ -45,11 +47,11 @@ namespace maschinendeck {
     public:
       Pair(T first, U second) : first_(first), second_(second) {}
       T first() {
-        return this->first_;  
+        return this->first_;
       }
 
       U second() {
-        return this->second_;  
+        return this->second_;
       }
   };
 
@@ -58,30 +60,35 @@ namespace maschinendeck {
       Command* commands[64];
       uint8_t size_;
       bool firstRun;
+      String message;
 
       #ifndef ST_FLAG_NOHELP
       void printCommands() {
-        for (uint8_t i = 0; i <= this->size_; i++) {
-          Serial.println("\t" + this->commands[i]->command + "\t" + this->commands[i]->description);  
-        }  
-      }
-      #endif
-      
-    public:
-      SerialTerminal(long baudrate) : size_(0), firstRun(true) {
-        #if not defined ST_FLAG_NOBUILTIN && defined E2END
-          this->add("eeprom", &printEEPROM, "prints the contents of EEPROM");
-        #endif     
-        Serial.begin(baudrate);
-        #ifndef ST_FLAG_NOHELP
-          Serial.print("SerialTerm v");
-          Serial.print(ST_VERSION);
-          Serial.print("\n");
-          Serial.println("(C) 2019, MikO");
-          Serial.println("  available commands:");
+        for (uint8_t i = 0; i < this->size_; i++) {
+          Serial.println("\t" + this->commands[i]->command + "\t" + this->commands[i]->description);
+        }
+        #ifndef ST_FLAG_NOPROMPT
+        Serial.print("st> ");
         #endif
       }
-      
+      #endif
+
+    public:
+      SerialTerminal(long baudrate = 0) : size_(0), firstRun(true), message("") {
+        #if not defined ST_FLAG_NOBUILTIN && defined E2END
+            this->add("eeprom", &printEEPROM, "prints the contents of EEPROM");
+        #endif
+        if (baudrate > 0)
+            Serial.begin(baudrate);
+        #ifndef ST_FLAG_NOHELP
+            Serial.print("SerialTerm v");
+            Serial.print(ST_VERSION);
+            Serial.print("\r\n");
+            Serial.println("(C) 2019, MikO");
+            Serial.println("  available commands:");
+        #endif
+      }
+
       void add(String command,  void(*callback)(String param), String description = "") {
         if (this->size_ >= 64)
           return;
@@ -90,7 +97,7 @@ namespace maschinendeck {
       }
 
       uint8_t size() {
-        return this->size_ + 1;  
+        return this->size_ + 1;
       }
 
       void loop() {
@@ -102,15 +109,47 @@ namespace maschinendeck {
         #endif
         if (!Serial.available())
           return;
-        String message = Serial.readString();
-        if (message == "")
-          return;
-        Pair<String, String> command = SerialTerminal::ParseCommand(message);
-        
-        for (uint8_t i = 0; i <= this->size_; i++) {
-          if (this->commands[i]->command == command.first())
-            this->commands[i]->callback(command.second()); 
+        bool commandComplete = false;
+        while (Serial.available()) {
+            char car = Serial.read();
+            if (car == 127 && this->message.length() > 0) {
+                Serial.print("\e[1D");
+                Serial.print(' ');
+                Serial.print("\e[1D");
+                this->message.remove(this->message.length() - 1);
+                continue;
+            }
+            if (isAscii(car))
+                Serial.print(car);
+            if (car == '\r') {
+                Serial.print("\r\n");
+                commandComplete = true;
+                break;
+            }
+            this->message += car;
         }
+
+        if (!commandComplete)
+            return;
+        if (this->message == "") {
+            #ifndef ST_FLAG_NOPROMPT
+            Serial.print("st> ");
+            #endif
+
+            return;
+        }
+
+        Pair<String, String> command = SerialTerminal::ParseCommand(this->message);
+        this->message = "";
+
+        for (uint8_t i = 0; i <= this->size_; i++) {
+          if (this->commands[i]->command == command.first()) {
+            this->commands[i]->callback(command.second());
+          }
+        }
+        #ifndef ST_FLAG_NOPROMPT
+        Serial.print("\r\nst> ");
+        #endif
       }
 
       static Pair<String, String> ParseCommand(String message) {
@@ -118,8 +157,9 @@ namespace maschinendeck {
         for (auto& car : message) {
           if (car == ' ')
             break;
-          keyword += car;  
+          keyword += car;
         }
+        if (keyword != "")
         message.remove(0, keyword.length());
         keyword.trim();
         message.trim();
@@ -127,5 +167,5 @@ namespace maschinendeck {
         return Pair<String, String>(keyword, message);
       }
   };
-  
+
 }
